c16dce68b6c4d438a6aaa8117ac0811fed6a0c98
Foley13 <folajimiopeyemisax13@gmail.com>
2024-08-19 23:53:11 +0100

    ...

--- pharmacy_be/settings.py
+++ pharmacy_be/settings.py
@@ -82,8 +82,14 @@ WSGI_APPLICATION = 'pharmacy_be.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "bluemoon",
+        "USER": "hakeem",
+        "PASSWORD": "rexonance",
+        "HOST": "localhost",
+        "PORT": 3306,
     }
 }
 
